cabc1821b46c69fa72fe265e8207eefd2d38c08d
Andrew Vavilin <andrew87.87@mail.ru>
2024-06-29 23:05:38 +0400

    Folder for headers

---
+++ include/convertjson.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+
+#pragma once
+using namespace std;
+struct RelativeIndex
+{
+    size_t doc_id;
+    float rank;
+    bool operator==(RelativeIndex &other) const;
+};
+class ConverterJSON
+{
+private:
+    int LimitResponses;
+    vector<string> Documents;
+    vector<string> Requests;
+
+public:
+    ConverterJSON() = default;
+    vector<string> GetTextDocuments(const string &file);
+    int GetResponsesLimit(const string &file, const auto &field_name);
+    vector<string> GetRequests(const string &file_request);
+    void putAnswers(vector<vector<RelativeIndex>> Answers);
+};
---
+++ include/searchserver.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+
+#pragma once
+using namespace std;
+
+struct compare
+{
+    bool operator()(const RelativeIndex &relative_index1, const RelativeIndex &relative_index2);
+};
+
+class SearchServer
+{
+
+private:
+    InvertedIndex _index;
+
+public:
+    SearchServer(InvertedIndex &idx) : _index(idx){};
+
+    vector<vector<RelativeIndex>> search(const vector<string> &queries_input);
+};
